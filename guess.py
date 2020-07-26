9b53eb82554f322311aac3f04709ed790b0f58b2
alex-ele-xela <62929473+alex-ele-xela@users.noreply.github.com>
2020-07-26 22:58:08 +0530

    Updated guess.py
    This update lets the user input the "low" and "high"
    It also contains a function "getnum" to accept only integer values
    
    Few changes are also made to the way messages are printed

--- guess.py
+++ guess.py
@@ -1,53 +1,76 @@
 ###############################
 # file guess.py               #
-# author Nanak Tattyrek       #
-# version 0.5                 #
+# authors Nanak Tattyrek      #
+#         alex-ele-xela       #
+# version 1.0                 #
 ###############################
 
 import random
-import sys
 
-low = 1
-high = 30
+
+def getnum(msg=''):
+    '''Takes input with printed message (msg)
+    Successfully returns a valid integer number and handles ValueError'''
+
+    try:
+        x = int(input(msg))
+    except ValueError:
+        print("Please enter an integer number. Try again")
+        return getnum(msg)
+    return x
+
+
+try:
+    while True:
+        #Accepts the low and high, while making sure that low < high
+
+        low = getnum("Enter the lower bound for the game - ")
+        high = getnum("Enter the upper bound for the game - ")
+
+        if low < high:
+            break
+        else:
+            print("Upper Bound must be greater than Lower Bound. Try Again\n")
+except KeyboardInterrupt:
+    print("\n^C detected, terminating...")
+    exit(0)
+
+
 guesslimit = 5
 
-print("Guess a number between {0} and {1}".format(low, high))
-print("You have {0} guesses to get the right number".format(guesslimit))
+print(f"\nGuess the number between {low} and {high}")
+print(f"You have {guesslimit} guesses to get the right number")
 
-guessnumb = 0
 number = random.randint(low, high)
 
 try:
-	while guessnumb < guesslimit:
-		try:
-			guess = int(input('What\'s your guess? '))
-		except ValueError:
-			print("Couldn\'t convert input to Integer")
-			continue
-	
-		if guess < low or guess > high:
-			print("Value out of range ({0} - {1})".format(low, high))
-		else:
-			guessnumb += 1
-			
-			if guess < number:
-				print("Your guess was too low")
-		
-			if guess > number:
-				print("Your guess was too high")
-		
-			if guess == number:
-				break
-	
+    while guesslimit:
+        '''The loop ends only if there are no more tries left (guesslimit = 0)
+        Or if the guess is correct'''
+        
+        guess = getnum("\nWhat's your guess? ")
+
+        if guess < low or guess > high:
+            print("Value out of range ({0} - {1})".format(low, high))
+
+        if guess < number:
+            print("Your guess was too low")
+        elif guess > number:
+            print("Your guess was too high")
+        else:
+            break
+
+        guesslimit -= 1
+
 except KeyboardInterrupt:
-	print("\n  ^C detected, terminating...")
-	sys.exit()
+    print("\n^C detected, terminating...")
+    exit(0)
 
 except EOFError:
-	print("\n  ^D detected, terminating...")
-	sys.exit()
+    print("\n^D detected, terminating...")
+    exit(0)
 
 if guess == number:
-	print("Great, you guessed the number in {0} trys".format(guessnumb))
+    print(f"\n{guess} is the Correct Answer!!\nGreat, you guessed the number in {6-guesslimit} tries")
 else:
-	print("No, I was thinking of the number {0}".format(number))
+    print(f"\nNo, I was thinking of the number {number}")
